e79b89908defc6819a18cde09cdf9d419a7507de
Kevin Darcy <neoglitch822@yahoo.com>
2019-10-29 22:31:43 -0400

    Marzipan Screwdriver - added buyProducts funciton, but, it skips the first prompt... need to find out how to wait for inputs before next functions executes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,17 +1,60 @@
 var mysql = require("mysql");
-var inquire = require("inquirer");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "12345",
     database: "my_databaseDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected as id : " + connection.threadId);
     connection.end();
 });
+
+function readProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+        // connection.end(); don't... just don't do this
+
+        inquirer
+            .prompt([{
+                type: "input",
+                name: "id",
+                message: "What item are you looking for? Search by ID: "
+            }])
+            .then(answers => {
+                console.log(answers.id);
+            });
+    });
+
+}
+readProducts();
+
+
+// ------------------- readProducts does NOT wait until you input it before it prompts for buyProducts, hmm...
+
+function buyProducts() {
+    console.log("What is the Product ID?\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+
+        inquirer
+            .prompt([{
+                type: "number",
+                name: "quantity",
+                message: "How many would you like?"
+            }])
+            .then(answers => {
+                console.log(answers.quantity);
+            });
+    });
+}
+buyProducts();
 
